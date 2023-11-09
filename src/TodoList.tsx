741ca61eaae5b3d114de114498b95902a44bf276
gyuhyuk <bruno3@naver.com>
2023-11-10 03:50:48 +0900

    #6.7 Form Validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,17 +1,45 @@
 import { useForm } from "react-hook-form";
 
 const TodoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onVaild = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("firstname")} placeholder="First Name" />
-        <input {...register("lastname")} placeholder="Last Name" />
-        <input {...register("username")} placeholder="Username" />
-        <input {...register("password")} placeholder="Password" />
-        <input {...register("password1")} placeholder="Password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onVaild)}
+      >
+        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstname", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastname", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 10 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "your password is too short",
+            },
+          })}
+          placeholder="Password1"
+        />
         <button>Add</button>
       </form>
     </div>
